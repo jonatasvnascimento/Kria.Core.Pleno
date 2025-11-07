009f2cd3978f0aa848cdfa9a6ee4d18bf0a46c83
Jonatas Viana <56093381+jonatasvnascimento@users.noreply.github.com>
2025-11-07 13:37:11 -0300

    Add PedagioDAO class for MongoDB interaction
    Introduced a new `PedagioDAO` class in the `Kria.Core.Pleno.Lib.DAO` namespace to interact with the "Pedagios" MongoDB collection. This class implements the `IPedagioDAO` interface and includes the `ObterTodos` method to retrieve all documents as a list of `Pedagio` objects. Added necessary `using` directives for MongoDB and other functionalities. Defined the `IPedagioDAO` interface, though it currently lacks method declarations.

---
+++ Kria.Core.Pleno.Lib/DAO/PedagioDAO.cs
@@ -0,0 +1,22 @@
+﻿using Kria.Core.Pleno.Lib.Context;
+using Kria.Core.Pleno.Lib.Entidades;
+using Kria.Core.Pleno.Lib.Interfaces.DAO;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kria.Core.Pleno.Lib.DAO
+{
+    public class PedagioDAO(MongoDbContext context) : IPedagioDAO
+    {
+        private readonly IMongoCollection<Pedagio> _context = context.GetCollection<Pedagio>("Pedagios");
+
+        public List<Pedagio> ObterTodos()
+        {
+            return _context.AsQueryable().ToList();
+        }
+    }
+}
---
+++ Kria.Core.Pleno.Lib/Interfaces/DAO/IPedagioDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kria.Core.Pleno.Lib.Interfaces.DAO
+{
+    public interface IPedagioDAO
+    {
+    }
+}
